33033d216685cb11065213a4a4f1d818d6a3e37e
Juan Martinez <juanmartinez5678@hotmail.com>
2023-11-27 15:18:16 -0500

    worked on the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import Header from './components/Header.tsx'
 
 function App() {
 
 
   return (
     <>
-  
+    <Header/>
     </>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import lifter from '../images/lifter.png'
+
+
+export default function Header(){
+
+    return(
+        <header>
+            <div className="background-color"></div>
+            <div className="background-color2"></div>
+            <nav>
+                <div className="logo">Page logo</div> 
+                <ul className="page-links">
+                    <li>HOME</li>
+                    <li>ABOUT</li>
+                    <li>PAGES</li>
+                    <li>GLASSES</li>
+                    <li>SHOP</li>
+                    <li>TRAINERS</li>
+                    <li>BLOGS</li>
+                    <li>CONTACT</li>
+                <div className="checkout">cart</div>
+
+                </ul>
+            </nav>
+            <div className="main-cont">
+              <div className="description">
+              <h2><span className="yellow">TIME</span> TO GET <span className="yellow">FIT</span></h2>
+              <p>The Gym is very important to maintain our health and to achieve our goals! </p>
+              <button className=' contact-btn'>Contact Now</button>
+
+              </div>
+                <img src={lifter} alt="main lifting weights" />
+            </div>
+        </header>
+    )
+}
