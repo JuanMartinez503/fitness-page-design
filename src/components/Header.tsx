752e97549f694b73c9ca2edb06f29ff773301f48
Juan Martinez <juanmartinez5678@hotmail.com>
2023-11-28 11:24:35 -0500

    worked on the image container

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,10 @@
 import lifter from '../images/lifter.png'
+import ImageContainer from './ImageContainer.tsx'
+import abWork from '../images/ab.jpg'
+import backWork from  '../images/back-work.jpg'
+import ropeWork from '../images/rope-work.jpg'
+import squatting from '../images/squatting.jpg'
+
 
 
 export default function Header(){
@@ -32,6 +38,13 @@ export default function Header(){
               </div>
                 <img src={lifter} alt="main lifting weights" />
             </div>
+            <div className="image-container">
+            <ImageContainer src={backWork} name='Back Workout' duration='3 SETS, 5minutes'/>
+            <ImageContainer src={squatting} name='Weight Lifting' duration='4 SETS, 5minutes'/>
+            <ImageContainer src={abWork} name='Abs Fitness' duration='3 SETS, 5 minutes'/>
+            <ImageContainer src={ropeWork} name='Ropes Workout' duration='3 SETS, 5minutes'/>
+            </div>
+
             </section>
          
         </header>
---
+++ src/components/ImageContainer.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+type ImageContainerProps = {
+  src: string;
+  name: string;
+  duration: string;
+};
+
+export default function ImageContainer({ src, name, duration }: ImageContainerProps) {
+  const containerStyle: React.CSSProperties = {
+    backgroundImage: `url(${src})`,
+  };
+
+  return (
+    <div className="image-container">
+      <div style={containerStyle}>
+        <h3 className="name">{name}</h3>
+        <p className="duration">{duration}</p>
+      </div>
+    </div>
+  );
+}
